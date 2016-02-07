336c52bd0b28d199a4cf7888ee46dd88c99b94e4
Shawn S Hillyer <shawn.hillyer@gmail.com>
2016-02-06 21:30:48 -0800

    Zip code working okay

--- js/app.js
+++ js/app.js
@@ -1,19 +1,57 @@
+/*
+Author: Shawn S Hillyer
+Date: 02-06-2016
+Course: CS290-400
+Assignment: Week6
+
+Note: Using the Bootstrap basic template for the HTML framework.
+Source: http://getbootstrap.com/getting-started/#template
+All javascript I wrote is in app.js for this assignment
+*/
+
+// I'm using the simple design pattern at http://eecs.oregonstate.edu/ecampus-video/CS290/core-content/ajax-forms/js-forms.html to add the event listener once DOm is loaded and bind the buttons, etc.
+
 "use strict";
 
-var req = new XMLHttpRequest();
+let apiKey = "fa7d80c48643dfadde2cced1b1be6ca1";
+let data = {};
 
-var queryString = "Corvallis,or";
-var apiKey = "fa7d80c48643dfadde2cced1b1be6ca1";
+function bindButtons() {
+    document.getElementById('zip-submit').addEventListener('click', function(event){
+        let req = new XMLHttpRequest();
 
-req.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + queryString + "&appid=" + apiKey, false);
-// console.log("Sending: " );
-req.send(null);
+        // Build the first part of the query string
+        let queryString = "q=" + document.getElementById("zip-code").value + ",us";
+        
+        
+        // Send the GET request with the zip code
+        req.open("GET", "http://api.openweathermap.org/data/2.5/weather?" + queryString + "&appid=" + apiKey, false);
+        req.send(null);
 
-var weather = JSON.parse(req.responseText);
-console.log(weather);
+        data = JSON.parse(req.responseText);
+        console.log(data);
 
-
-console.log("The object returned contains these properties: ");
-for (let property in weather) {
-    console.log(property);
+        console.log("The object returned contains these properties: ");
+        for (let property in data) {
+            console.log(property);
+        }
+        
+        updateWeatherInfo(data);
+        
+        event.preventDefault(); // prevent page from reloading on click of form submit button
+    })
 }
+
+
+function updateWeatherInfo(info) {
+    console.log(data.name);
+}
+
+
+document.addEventListener('DOMContentLoaded', bindButtons);
+
+
+// data.weather[0].description  retreives a short description
+// data.coord.lon  data.coord.lat   --> longitude and latitude
+// data.wind.speed
+// data.name -- gives county
